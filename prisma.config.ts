5368426e71fb0cdf50d7c2873d91e1d1fdf42d6e
YUDISTIRA <147828921+yudistira1706@users.noreply.github.com>
2026-06-12 19:37:31 +0700

    chore: enforce library engine type via schema and vercel env variables

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from '@prisma/config';
 export default defineConfig({
   schema: './prisma/schema.prisma',
   datasource: {
-    url: process.env.DATABASE_URL, 
+    url: process.env.DATABASE_URL,
   },
 });
